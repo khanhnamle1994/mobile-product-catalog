4aad123c5169fd345367708a39f003340b6d6a91
James Le <le_j6@denison.edu>
2017-07-25 03:02:57 -0700

    Ionic part of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { CategoriesList } from '../pages/categories_list/categories_list';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
